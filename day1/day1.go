528009bd591d2f376d23e649b2fbf314a6e5a71b
dsaintsurin <daelsaintsurin@gmail.com>
2024-05-03 09:49:56 -0400

    Day 1 Part 2 complete

--- day1/day1.go
+++ day1/day1.go
@@ -4,9 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
-	"unicode"
+	"strings"
 )
 
 func Solution(file string) {
@@ -24,18 +25,44 @@ func Solution(file string) {
 }
 
 func helper(str string) int {
-	var c1 rune
-	var c2 rune
-	for _, c := range str {
-		if unicode.IsDigit(c) {
-			if c1 == 0 {
-				c1 = c
-			}
-			c2 = c
+	str2digit := map[string]int{
+		"zero":  0,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+	digits := "0123456789"
+	strFIdx := math.MaxInt
+	fNum := -1
+	strLIdx := -math.MaxInt
+	lNum := -1
+	for key := range str2digit {
+		if i := strings.Index(str, key); i < strFIdx && i != -1 {
+			strFIdx = i
+			fNum = str2digit[key]
+
+		}
+		if i := strings.LastIndex(str, key); i > strLIdx && i != -1 {
+			strLIdx = i
+			lNum = str2digit[key]
 		}
 	}
 
-	k := fmt.Sprintf("%c%c", c1, c2)
-	num, _ := strconv.Atoi(k)
-	return num
+	numFIdx := strings.IndexAny(str, digits)
+	if numFIdx < strFIdx {
+		fNum, _ = strconv.Atoi(fmt.Sprintf("%c", str[numFIdx]))
+	}
+
+	numLIdx := strings.LastIndexAny(str, digits)
+
+	if numLIdx > strLIdx {
+		lNum, _ = strconv.Atoi(fmt.Sprintf("%c", str[numLIdx]))
+	}
+	return 10*fNum + lNum
 }
