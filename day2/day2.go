3e0a36d716187f56a4abd621a50aaf75c49326ec
dsaintsurin <daelsaintsurin@gmail.com>
2024-05-04 18:49:12 -0400

    Day-2 Done!

--- day2/day2.go
+++ day2/day2.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -24,15 +25,33 @@ func Solution(file string) {
 
 	scanner := bufio.NewScanner(f)
 	sum := 0
+	sum_2 := 0
 	for scanner.Scan() {
 
 		inputString := scanner.Text()
 
 		gameNumber := helper(inputString)
 
+		productGameNumber := day2(inputString)
 		sum += gameNumber
+		sum_2 += productGameNumber
 	}
-	fmt.Println(sum)
+	fmt.Printf("Day 1: %d\nDay 2: %d\n", sum, sum_2)
+}
+
+func day2(str string) int {
+	_, plays, err := parseInput(str)
+	if err != nil {
+		fmt.Println("Error parsing input", err)
+		return 0
+	}
+	if err != nil {
+		fmt.Println("Error parsing input", err)
+		return 0
+	}
+
+	return getProductMinimumCubes(plays)
+
 }
 
 func helper(str string) int {
@@ -89,6 +108,38 @@ func checkCubeAvailability(cubes Cubes, plays []string) bool {
 		}
 	}
 	return true
+}
+
+func getProductMinimumCubes(plays []string) int {
+	maxRed := math.MinInt
+	maxGreen := math.MinInt
+	maxBlue := math.MinInt
+	for _, play := range plays {
+		play = strings.TrimSpace(play)
+		hands := strings.Split(play, ",")
+		for _, hand := range hands {
+			hand = strings.TrimSpace(hand)
+			parts := strings.Split(hand, " ")
+			count, _ := strconv.Atoi(parts[0])
+			color := parts[1]
+			switch color {
+			case "red":
+				if maxRed < count {
+					maxRed = count
+				}
+			case "green":
+				if maxGreen < count {
+					maxGreen = count
+				}
+			case "blue":
+				if maxBlue < count {
+					maxBlue = count
+				}
+			}
+
+		}
+	}
+	return maxRed * maxGreen * maxBlue
 }
 
 func parseInput(input string) (int, []string, error) {
